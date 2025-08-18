a568c269ddd85a94e0aceec0e2c016e88661500b
= <mahcara87@gmail.com>
2025-08-18 21:14:20 +0100

    all

--- landingpages/urls.py
+++ landingpages/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path("landing-pages/", views.landingpage_list, name="list"),
     path("create/<product_id>/", views.landingpage_create, name="create"),
     path("<str:pk>/", views.landingpage_detail, name="detail"),
-    path("<int:pk>/edit/", views.landingpage_edit, name="edit"),
-    path("<int:pk>/delete/", views.landingpage_delete, name="delete"),
+    path("<str:pk>/edit/", views.landingpage_edit, name="edit"),
+    path("<str:pk>/delete/", views.landingpage_delete, name="delete"),
 
 ]
 

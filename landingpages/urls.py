997fe35a18de0e7184028500ddd2d696a6706bed
= <mahcara87@gmail.com>
2025-08-18 21:13:46 +0100

    all

--- landingpages/urls.py
+++ landingpages/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("manage_cities/", views.manage_cities, name="manage_cities"),
     path("landing-pages/", views.landingpage_list, name="list"),
     path("create/<product_id>/", views.landingpage_create, name="create"),
-    path("<int:pk>/", views.landingpage_detail, name="detail"),
+    path("<str:pk>/", views.landingpage_detail, name="detail"),
     path("<int:pk>/edit/", views.landingpage_edit, name="edit"),
     path("<int:pk>/delete/", views.landingpage_delete, name="delete"),
 
